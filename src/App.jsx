f06808def6cbbf2c6660c9c97c64c1b7742ea0b9
tany0812 <chaitanyachangala8@gmail.com>
2025-02-25 14:40:10 -0500

    tailwind nav bar check in

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import './index.css';
 import './App.css'
+import {User,MessageCircle} from "lucide-react"
+
+
+const ProfileSelector =()=>{
+  return (
+    <div className='rounded-lg bg-gray-100 p-4 overflow-hidden'>
+        <div className='relative'> 
+          <img src='https://randomuser.me/api/portraits' />
+          <div className='absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 p-4 text-white'> 
+             <h2>Chaitanya Changala</h2>
+          </div>
+        </div>
+    </div>
+  );
+};
 
 function App() {
   return (
     <>
-    <p>Hello World</p>
+      <div className='max-w-md mx-auto'>
+        <nav className='flex justify-between items-center p-4 bg-gray-800 text-white'>
+          <User />
+          <MessageCircle />
+        </nav>
+        <ProfileSelector />
+      </div>
     </>
   )
 }
 
-export default App
+export default App;
