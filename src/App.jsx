1da41b2afe21e0109639206c4189163bc65b2603
tany0812 <chaitanyachangala8@gmail.com>
2025-02-26 11:51:23 -0500

    Add matches list page

--- src/App.jsx
+++ src/App.jsx
@@ -29,15 +29,41 @@ const ProfileSelector =()=>{
   );
 };
 
+const Matches =()=>{
+
+  return (
+    <div className='rounded-lg shadow-lg p-4'>
+    <h2 className='text-2xl font-bold'>Matches</h2>
+    <ul>
+    {[
+      {id:1, firstName:'Rahul',lastName:"Raj", photo:"http://127.0.0.1:8080/01f4c42f-86cb-45e6-ad2b-05430afb3639.jpg"},
+      {id:2, firstName:'Rahul',lastName:"sony", photo:"http://127.0.0.1:8080/03401693-2cfa-47f6-ba6a-5581c4791f65.jpg"},
+    ].map((match) => (
+      <li key={match.id} className='mb-2'>
+          <button className='w-full flex items-center space-x-4 hover:bg-gray-200 p-4 rounded-lg'>
+            <img src={match.photo
+            } className='w-16 h-16 rounded-full'/>
+            <span>
+              <h3 className='font-bold'>{match.firstName} {match.lastName} </h3>
+            </span>
+          </button>
+        </li>
+    ))}
+    </ul>
+    
+    </div>
+  )
+}
 function App() {
   return (
     <>
       <div className='max-w-md mx-auto'>
         <nav className='flex justify-between items-center p-4 bg-gray-800 text-white'>
           <User />
-          <MessageCircle />
+          <MessageCircle/>
         </nav>
-        <ProfileSelector />
+       {/* <ProfileSelector />*/}
+        <Matches/>
       </div>
     </>
   )
